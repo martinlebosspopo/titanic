666dd751bd75dc5d1e740126ad419dd1e455cdfb
martindufour <martin.dufour@sap.com>
2022-05-25 18:37:40 +0200

    New KNN Imputer

--- utils/transformers/base.py
+++ utils/transformers/base.py
@@ -1,5 +1,7 @@
 from sklearn.base import BaseEstimator, TransformerMixin
 
+import pandas as pd
+
 
 class BaseTransformer(BaseEstimator, TransformerMixin):
     def fit(self, X, y=None):
@@ -18,4 +20,28 @@ class ColumnsAsParameters(BaseTransformer):
     def fit(self, X, y=None):
         self.cols_final = self.cols_select or [col for col in X.columns if
                                                col not in self.cols_ignore]
+        self.cols_ignore = [col for col in X.columns if col not in self.cols_final]
         return self
+
+    def split_ignored_final(self, X):
+        return X[self.cols_ignore], X[self.cols_final]
+
+    def format_df(self, X, X_transformed_arr):
+        df_tr = pd.DataFrame(X_transformed_arr, columns=self.cols_final, index=X.index)
+        return pd.concat([X[self.cols_ignore], df_tr], axis=1)
+
+
+class PandasTransformer(ColumnsAsParameters):
+    def __init__(self, cols_ignore=[], cols_select=[]):
+        super().__init__(cols_select=cols_select, cols_ignore=cols_ignore)
+        self.skmodel = None
+
+    def fit(self, X, y=None):
+        super().fit(X)
+        self.skmodel.fit(X[self.cols_final])
+        return self
+
+    def transform(self, X, y=None):
+        X_ignored, X_cols_final = self.split_ignored_final(X)
+        X_transformed_arr = self.skmodel.transform(X_cols_final)
+        return self.format_df(X, X_transformed_arr)
--- utils/transformers/transformers.py
+++ utils/transformers/transformers.py
@@ -4,8 +4,7 @@ from sklearn.preprocessing import FunctionTransformer
 
 
 class Cabin(BaseTransformer):
-    def __init__(self, test_val=-1):
-        self.test_val = test_val
+    def __init__(self):
         self.cabins_known = None
 
     def get_cabins(self, X):
@@ -16,7 +15,6 @@ class Cabin(BaseTransformer):
         return self
 
     def transform(self, X, y=None):
-        X = X.copy()
         cabins = self.get_cabins(X)
         unknown_cabins = set(cabins) - self.cabins_known
         X['Cabin'] = cabins.replace(unknown_cabins, float('NaN'))
@@ -24,23 +22,9 @@ class Cabin(BaseTransformer):
 
 
 class SetupFeatures(ColumnsAsParameters):
-
-    def __init__(self, cols_ignore=[], cols_select=[]):
-        self.cols_ignore = cols_ignore
-        self.cols_select = cols_select
-        self.cols_final = None
-
-    def fit(self, X, y=None):
-        self.cols_final = self.cols_select or [col for col in X.columns if
-                                               col not in self.cols_ignore]
-        return self
-
     def transform(self, X, y=None):
-        X = X.copy()
-        self.cols_final = self.cols_final
         missing_cols = [col for col in self.cols_final if col not in X.columns]
         supp_cols = [col for col in X.columns if col not in self.cols_final]
-
         X = X.set_index(supp_cols)
         X[missing_cols] = float('NaN')
         return X[self.cols_final]
@@ -60,7 +44,6 @@ class ClipOutliers(ColumnsAsParameters):
         return self
 
     def transform(self, X, y=None):
-        X = X.copy()
         X[self.cols_final] = X[self.cols_final].clip(
             lower=self.means - self.std_band * self.stds,
             upper=self.means + self.std_band * self.stds,
@@ -69,26 +52,11 @@ class ClipOutliers(ColumnsAsParameters):
         return X
 
 
-def cust_func(coltypes_overwrite):
-    col_types = AsTypes.COL_TYPES.copy()
-    col_types.update(coltypes_overwrite)
-
-    def func(X):
-        for col, dtype in col_types.items():
-            try:
-                X = X.astype({col:dtype})
-            except KeyError as err:
-                pass
-        return X
-
-    return func
-
-
 class AsTypes(FunctionTransformer):
     COL_TYPES = {
         'Pclass': 'category',
         'Sex': 'category',
-        'Age': 'int64',
+        'Age': 'float',
         'SibSp': 'category',
         'Parch': 'category',
         'Fare': 'float64',
@@ -99,4 +67,18 @@ class AsTypes(FunctionTransformer):
     def __init__(self, coltypes_overwrite={}):
         super().__init__()
         self.coltypes_overwrite = coltypes_overwrite
-        self.func = cust_func(coltypes_overwrite)
+        self.func = self.cust_func(coltypes_overwrite)
+
+    def cust_func(self, coltypes_overwrite):
+        col_types = AsTypes.COL_TYPES.copy()
+        col_types.update(coltypes_overwrite)
+
+        def func(X):
+            for col, dtype in col_types.items():
+                try:
+                    X = X.astype({col: dtype})
+                except KeyError as err:
+                    pass
+            return X
+
+        return func
